8c172c8433de3662753dcb469a6e8af3234e5cb0
Quinten Roets <62651391+quintenroets@users.noreply.github.com>
2024-10-20 23:19:30 -0700

    support max python version (#64)

--- tests/utils/test_package_info.py
+++ tests/utils/test_package_info.py
@@ -1,15 +1,28 @@
+from collections.abc import Iterator
+
 import pytest
 
 from package_dev_tools.models import Path
 from package_dev_tools.utils.package import PackageInfo
 
 required_python_version = "3.10"
-supported_python_versions = ["3.10", "3.11", "3.12"]
+supported_python_versions = ["3.10", "3.11", "3.12", "3.13"]
 
 
 @pytest.fixture
 def package_info(repository_path: Path) -> PackageInfo:
     return PackageInfo(repository_path)
+
+
+@pytest.fixture
+def package_info_with_max_version(repository_path: Path) -> Iterator[PackageInfo]:
+    name = "pyproject.toml"
+    with Path.tempdir() as directory:
+        old = 'requires-python = ">=3.10"'
+        new = 'requires-python = ">=3.10, <3.13"'
+        path = directory / name
+        path.text = (repository_path / name).text.replace(old, new)
+        yield PackageInfo(directory)
 
 
 def test_package_name(package_info: PackageInfo) -> None:
@@ -26,6 +39,13 @@ def test_required_python_version(package_info: PackageInfo) -> None:
 
 def test_supported_python_versions(package_info: PackageInfo) -> None:
     assert list(package_info.supported_python_versions) == supported_python_versions
+
+
+def test_supported_python_versions_with_max_version(
+    package_info_with_max_version: PackageInfo,
+) -> None:
+    versions = package_info_with_max_version.supported_python_versions
+    assert list(versions) == supported_python_versions[:-1]
 
 
 def test_supported_operating_systems(package_info: PackageInfo) -> None:
