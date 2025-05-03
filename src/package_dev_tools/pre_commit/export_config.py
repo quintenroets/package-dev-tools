31e4b9bdf288b97d932dc9b93083e4a020889010
Quinten <quinten.roets@gmail.com>
2025-05-03 14:31:33 -0700

    mypy

--- src/package_dev_tools/pre_commit/export_config.py
+++ src/package_dev_tools/pre_commit/export_config.py
@@ -1,4 +1,4 @@
-from typing import Any, cast
+from typing import cast
 
 from superpathlib import Path
 
@@ -7,8 +7,11 @@ def export_config() -> None:
     config_file = Path(".pre-commit-config.yaml")
     seed_file = Path(".pre-commit-seed.yaml")
     if seed_file.exists():
-        hooks = cast("list[dict[str, str]]", seed_file.yaml)
-        defaults = {"pass_filenames": False, "language": "system"}
+        hooks = cast("list[dict[str, str | bool]]", seed_file.yaml)
+        defaults: dict[str, str | bool] = {
+            "pass_filenames": False,
+            "language": "system",
+        }
         default_copies = {"id": "entry", "name": "id"}
         for hook in hooks:
             for k, v in defaults.items():
