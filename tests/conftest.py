8387c15005a82a764c8a3a97d5edceb081542155
Quinten <quinten.roets@gmail.com>
2025-05-03 11:26:59 -0700

    fix coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -72,7 +72,8 @@ def download_repository(
     if depth is not None:
         command = (*command, "--depth", depth)
     git_interface.capture_output(*command)
-    git_interface.capture_output("checkout ci")
+    if "template" in name:
+        git_interface.capture_output("checkout ci")
 
 
 def generate_coverage_results(path: Path) -> None:
