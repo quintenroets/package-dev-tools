9f3184ee8cb8995857ff6da53e5c6c9d92ba036e
Quinten <quinten.roets@gmail.com>
2025-05-03 14:25:11 -0700

    use validate

--- src/package_dev_tools/pre_commit/export_config.py
+++ src/package_dev_tools/pre_commit/export_config.py
@@ -7,13 +7,14 @@ def export_config() -> None:
     config_file = Path(".pre-commit-config.yaml")
     seed_file = Path(".pre-commit-seed.yaml")
     if seed_file.exists():
-        config = cast("dict[str, list[dict[str, list[Any]]]]", seed_file.yaml)
-        hooks = cast("list[dict[str, str]]", config["repos"][0]["hooks"])
+        hooks = cast("list[dict[str, str]]", seed_file.yaml)
+        defaults = {"pass_filenames": False, "language": "system"}
+        default_copies = {"id": "entry", "name": "id"}
         for hook in hooks:
-            if "language" not in hook:
-                hook["language"] = "system"
-            if "id" not in hook:
-                hook["id"] = hook["entry"]
-            if "name" not in hook:
-                hook["name"] = hook["id"]
-        config_file.yaml = config
+            for k, v in defaults.items():
+                if k not in hook:
+                    hook[k] = v
+            for k, v in default_copies.items():
+                if k not in hook:
+                    hook[k] = hook[v]
+        config_file.yaml = {"repos": [{"hooks": hooks}]}
